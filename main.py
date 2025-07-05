cb05e596bcc0a0f40117da0072b2a4169bbf6987
merlinrts <merlin.rts@gmail.com>
2025-07-05 18:33:53 +0200

    refactoring

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from LcdDriver import LcdDriver
 if __name__ == "__main__":
     print("Starting main")
     lcdDisplay = LcdDriver()
-    lcdDisplay.display_message(" Rasenballsport ")
-    lcdDisplay.display_message("    LEIPZIG     ")
+    lcdDisplay.display_message(
+        " Rasenballsport ",
+        "    LEIPZIG     ")
     time.sleep(10)
     lcdDisplay.clear_display()
     print("Ending main")

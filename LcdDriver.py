1b5a6e13ad738b9bc80f1b21db330a2384df60cc
merlinrts <merlin.rts@gmail.com>
2025-07-05 19:24:02 +0200

    add moving messages

--- LcdDriver.py
+++ LcdDriver.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('/home/knxpi/I2C-LCD')  # Der Pfad zum I2C-LCD-Verzeichnis
 import lcddriver  # Importiere das lcddriver-Modul
+import time
 
 
 class LcdDriver:
@@ -9,11 +10,24 @@ class LcdDriver:
 
     def display_message(self, message_1, message_2) -> None:
         if message_1 is None:
-            message_1 = "Message 1 empty"
+            message_1 = "Msg 1 empty"
         if message_2 is None:
-            message_2 = "Message 2 empty"
+            message_2 = "Msg 2 empty"
+        if len(message_1) > 16:
+            message_1 = "Msg 1 too long"
+        if len(message_2) > 16:
+            message_2 = "Msg 2 too long"
+
         self.lcd.lcd_display_string(message_1, 1)
         self.lcd.lcd_display_string(message_2, 2)
+
+
+    def display_mooving_message(self, message: str) -> None:
+        message = f"                {message}                "
+        for i in range(len(message) - 16):
+            self.lcd.lcd_display_string(message[i:i + 16], 1)
+            time.sleep(0.25)
+
 
     def clear_display(self) -> None:
         self.lcd.lcd_clear()
--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from LcdDriver import LcdDriver
 if __name__ == "__main__":
     print("Starting main")
     lcdDisplay = LcdDriver()
-    lcdDisplay.display_message(
-        " Rasenballsport ",
-        "    LEIPZIG     ")
-    time.sleep(10)
+    lcdDisplay.display_mooving_message("Ein Schuss - Ein Tor - Für Leipzig")
     lcdDisplay.clear_display()
     print("Ending main")

8f884ebb4bb32d9a323d22b76bd78b3490b132e0
merlinrts <merlin.rts@gmail.com>
2025-07-05 18:32:48 +0200

    refactoring

--- LcdDriver.py
+++ LcdDriver.py
@@ -5,15 +5,15 @@ import lcddriver  # Importiere das lcddriver-Modul
 
 class LcdDriver:
     def __init__(self):
-        lcd = lcddriver.lcd()
+        self.lcd = lcddriver.lcd()
 
     def display_message(self, message_1, message_2) -> None:
         if message_1 is None:
             message_1 = "Message 1 empty"
         if message_2 is None:
             message_2 = "Message 2 empty"
-        lcd.lcd_display_string(message_1, 1)
-        lcd.lcd_display_string(message_2, 2)
+        self.lcd.lcd_display_string(message_1, 1)
+        self.lcd.lcd_display_string(message_2, 2)
 
     def clear_display(self) -> None:
-        lcd.lcd_clear()
+        self.lcd.lcd_clear()
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from LcdDriver import LcdDriver
 
 if __name__ == "__main__":
     print("Starting main")
-    lcd = LcdDriver()
-    lcd.display_message(" Rasenballsport ")
-    lcd.display_message("    LEIPZIG     ")
+    lcdDisplay = LcdDriver()
+    lcdDisplay.display_message(" Rasenballsport ")
+    lcdDisplay.display_message("    LEIPZIG     ")
     time.sleep(10)
-    lcd.clear_display()
+    lcdDisplay.clear_display()
     print("Ending main")

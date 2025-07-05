774e7e8b9461ba1954c6482e1569b6bcf86bb0fa
merlinrts <merlin.rts@gmail.com>
2025-07-05 13:34:41 +0200

    added main

---
+++ main.py
@@ -0,0 +1,17 @@
+# lcd_display.py
+import I2C_LCD_driver
+from time import sleep
+
+mylcd = I2C_LCD_driver.lcd()
+
+def display_hello_world():
+    while True:
+        mylcd.lcd_display_string("Hello, World!!!!", 1)
+        mylcd.lcd_display_string("RPi + LCD", 2)
+
+        sleep(1)
+        mylcd.lcd_clear()
+        sleep(1)
+
+if __name__ == "__main__":
+    display_hello_world()
